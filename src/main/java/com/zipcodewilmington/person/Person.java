3f4061208dc6f50d88fb3df6b330a06e2470271d
Loc <loc@Zipcoders-MacBook-Pro-2.local>
2023-11-01 16:56:45 -0400

    setters work

--- src/main/java/com/zipcodewilmington/person/Person.java
+++ src/main/java/com/zipcodewilmington/person/Person.java
@@ -20,12 +20,15 @@ public class Person {
     }
 
     public Person(String name, int age) {
+
     }
 
     public void setName(String name) {
+        this.name = name;
     }
 
     public void setAge(int age) {
+        this.age = age;
     }
 
     public String getName() {
